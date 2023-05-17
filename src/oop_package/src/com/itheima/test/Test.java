d978b0a35ec624dd4de9a3060f2a71f58f2577d0
huahua <2910115173@qq.com>
2023-05-17 19:29:06 +0800

    包的使用，同类不同包，不同类同包、不同类不同包的具体使用

---
+++ src/oop_package/src/com/itheima/test/Test.java
@@ -0,0 +1,24 @@
+package oop_package.src.com.itheima.test;
+
+
+import oop_package.src.com.itheima.domain2.Teacher;
+
+public class Test {
+    public static void main(String[] args) {
+        //创建对象
+        /*Student s = new Student();
+        s.setName("张三");
+        s.setAge(23);
+
+        System.out.println(s.getName() + ", " + s.getAge());*/
+
+
+      /*  String s = "abc";
+        System.out.println(s);*/
+
+
+        oop_package.src.com.itheima.domain1.Teacher t = new oop_package.src.com.itheima.domain1.Teacher();
+        Teacher t2 = new Teacher();
+
+    }
+}
