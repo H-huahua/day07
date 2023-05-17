3153df441af00c9a75aa3fc1a5e421364692216c
huahua <2910115173@qq.com>
2023-05-17 19:27:16 +0800

    权限修饰符的demo案例

---
+++ src/oop_jurisdiction/src/com/itheima/demo2/Dog.java
@@ -0,0 +1,10 @@
+package oop_jurisdiction.src.com.itheima.demo2;
+
+import oop_jurisdiction.src.com.itheima.demo1.Animal;
+
+public class Dog extends Animal {
+
+    public void show() {
+        System.out.println(name);
+    }
+}
---
+++ src/oop_jurisdiction/src/com/itheima/demo2/Test.java
@@ -0,0 +1,11 @@
+package oop_jurisdiction.src.com.itheima.demo2;
+
+
+import oop_jurisdiction.src.com.itheima.demo1.Animal;
+
+public class Test {
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        System.out.println(a.name);
+    }
+}
